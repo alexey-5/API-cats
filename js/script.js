4691ae36cd70272e013c03a2bb6e942c493cc6f4
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2023-01-19 11:18:03 +0300

    добавил собак

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-const url = 'https://aws.random.cat/meow';
+const url1 = 'https://aws.random.cat/meow';
+const url2 = 'https://dog.ceo/api/breeds/image/random';
+let urlEnd = 'https://aws.random.cat/meow';
+
 const image = document.querySelector('.img');
-const button = document.querySelector('.btn')
+const button = document.querySelector('.btn1')
+const buttonTopic = document.querySelector('.btn2')
+
+let topic = true // коты 
 
 async function fetchRes() {
    try {
-     button.disabled = true;
-      const response = await fetch(url);
+      button.disabled = true;
+      const response = await fetch(urlEnd);
       const data = await response.json();
-      console.log(data)
-      image.src = data.file;
+      console.log(data);
+
+        if (topic === true)  image.src = data.file;
+         else image.src = data.message;
       setTimeout(timeOut, 2500);
       //задержка позволяет дождаться отрисовки картины в браузере
    } catch (err) {
@@ -16,8 +24,20 @@ async function fetchRes() {
    }
 }
 
-function timeOut() { button.disabled = false }
-
+function timeOut() { button.disabled = false }// отключение кнопки
+function topicSelector() { // перек. темы
+   topic = !topic
+   if (topic === true) {
+      buttonTopic.innerHTML = 'Котики';
+      urlEnd = url1;
+   }
+   else {
+      buttonTopic.innerHTML = 'Пёсики';
+      urlEnd = url2;
+   }
+   fetchRes();
+}
 fetchRes();
 
 button.addEventListener("click", fetchRes);
+buttonTopic.addEventListener("click", topicSelector);
